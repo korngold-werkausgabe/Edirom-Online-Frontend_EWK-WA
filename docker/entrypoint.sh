d49ea5dc532b0be0e39e2ad91f79a9635c85490e
Silke Reich <34301522+Diginaut@users.noreply.github.com>
2026-02-04 15:07:58 +0100

    Dynamic backend path and url

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -18,21 +18,31 @@ echo "APP_PATH environment variable: '${APP_PATH}'"
 echo "Normalized path: '${NORMALIZED_PATH}'"
 echo "====================================="
 
-PLACEHOLDER="/APP_PATH"
+APP_PATH="/APP_PATH"
+BACKEND_PATH="/BACKEND_PATH"
+BACKEND_URL="BACKEND_URL"
 
 # Replace placeholder in build files
-echo "Replacing placeholder '${PLACEHOLDER}' with '${NORMALIZED_PATH}' in built files..."
+echo "Replacing placeholder '${APP_PATH}' with '${NORMALIZED_PATH}' in built files..."
 
 find /usr/share/nginx/html \
   -type f \( -name "*.html" -o -name "*.js" -o -name "*.css" \) -print0 \
 | while IFS= read -r -d '' f; do
-  sed -i "s|${PLACEHOLDER}/|${NORMALIZED_PATH%/}/|g" "$f" # %/ removes trailing slash for correct replacement
-  sed -i "s|${PLACEHOLDER}|${NORMALIZED_PATH}|g" "$f"
+  sed -i "s|${APP_PATH}/|${NORMALIZED_PATH%/}/|g" "$f" # %/ removes trailing slash for correct replacement
+  sed -i "s|${APP_PATH}|${NORMALIZED_PATH}|g" "$f"
+  sed -i "s|${BACKEND_PATH}/|${BACKEND_PATH%/}/|g" "$f" # %/ removes trailing slash for correct replacement
+  sed -i "s|${BACKEND_PATH}|${BACKEND_PATH}|g" "$f"
+  sed -i "s|${BACKEND_URL}/|${BACKEND_URL%/}/|g" "$f" # %/ removes trailing slash for correct replacement
+  sed -i "s|${BACKEND_URL}|${BACKEND_URL}|g" "$f"
 done
 
 # replace placeholder in nginx configuration
-sed -i "s|${PLACEHOLDER}/|${NORMALIZED_PATH%/}/|g" /etc/nginx/nginx.conf # %/ removes trailing slash for correct replacement
-sed -i "s|${PLACEHOLDER}|${NORMALIZED_PATH}|g" /etc/nginx/nginx.conf
+sed -i "s|${APP_PATH}/|${NORMALIZED_PATH%/}/|g" /etc/nginx/nginx.conf # %/ removes trailing slash for correct replacement
+sed -i "s|${APP_PATH}|${NORMALIZED_PATH}|g" /etc/nginx/nginx.conf
+sed -i "s|${BACKEND_PATH}/|${BACKEND_PATH%/}/|g" /etc/nginx/nginx.conf # %/ removes trailing slash for correct replacement
+sed -i "s|${BACKEND_PATH}|${BACKEND_PATH}|g" /etc/nginx/nginx.conf
+sed -i "s|${BACKEND_URL}/|${BACKEND_URL%/}/|g" /etc/nginx/nginx.conf # %/ removes trailing slash for correct replacement
+sed -i "s|${BACKEND_URL}|${BACKEND_URL}|g" /etc/nginx/nginx.conf
 
 echo "Placeholder replacement completed."
 echo "====================================="
